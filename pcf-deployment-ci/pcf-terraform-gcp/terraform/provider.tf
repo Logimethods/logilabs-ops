a6dd11c60f8faa3bcb4c5d0ba05806c2f1853d42
Igor Fomenko <igor.fomenko@logimethods.com>
2017-05-18 15:30:13 -0400

    added variable file to terraform

--- pcf-deployment-ci/pcf-terraform-gcp/terraform/provider.tf
+++ pcf-deployment-ci/pcf-terraform-gcp/terraform/provider.tf
@@ -2,5 +2,5 @@ provider "google" {
   project     = "${var.project}"
   region      = "${var.region}"
 }
-ssl_cert="${ssl_cert}"
-ssl_cert_private_key="${private_key}"
+ssl_cert="test"
+ssl_cert_private_key="test"
