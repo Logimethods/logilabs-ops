ae0d4d739591802b5afbcd2e5d9159c627e9f1e3
Igor Fomenko <igor.fomenko@logimethods.com>
2018-01-17 09:48:26 -0500

    Changing file permissions

--- pcf-deployment-ci/pcf-terraform-gcp/ci/tasks/config-director/task.sh
+++ pcf-deployment-ci/pcf-terraform-gcp/ci/tasks/config-director/task.sh
@@ -45,21 +45,6 @@ network_configuration=$(
     --arg infra_dns "10.0.0.1,8.8.8.8" \
     --arg infra_gateway "192.168.101.1" \
     --arg infra_availability_zones "$availability_zones" \
-    --arg deployment_network_name "ert" \
-    --arg deployment_vcenter_network "${GCP_RESOURCE_PREFIX}-virt-net/${GCP_RESOURCE_PREFIX}-subnet-ert-${GCP_REGION}/${GCP_REGION}" \
-    --arg deployment_network_cidr "192.168.16.0/22" \
-    --arg deployment_reserved_ip_ranges "192.168.16.1-192.168.16.9" \
-    --arg deployment_dns "192.168.16.1,8.8.8.8" \
-    --arg deployment_gateway "192.168.16.1" \
-    --arg deployment_availability_zones "$availability_zones" \
-    --argjson services_network_is_service_network true \
-    --arg services_network_name "services-1" \
-    --arg services_vcenter_network "${GCP_RESOURCE_PREFIX}-virt-net/${GCP_RESOURCE_PREFIX}-subnet-services-1-${GCP_REGION}/${GCP_REGION}" \
-    --arg services_network_cidr "192.168.20.0/22" \
-    --arg services_reserved_ip_ranges "192.168.20.1-192.168.20.9" \
-    --arg services_dns "192.168.20.1,8.8.8.8" \
-    --arg services_gateway "192.168.20.1" \
-    --arg services_availability_zones "$availability_zones" \
     '
     {
       "icmp_checks_enabled": $icmp_checks_enabled,
@@ -75,34 +60,6 @@ network_configuration=$(
               "dns": $infra_dns,
               "gateway": $infra_gateway,
               "availability_zones": ($infra_availability_zones | split(","))
-            }
-          ]
-        },
-        {
-          "name": $deployment_network_name,
-          "service_network": false,
-          "subnets": [
-            {
-              "iaas_identifier": $deployment_vcenter_network,
-              "cidr": $deployment_network_cidr,
-              "reserved_ip_ranges": $deployment_reserved_ip_ranges,
-              "dns": $deployment_dns,
-              "gateway": $deployment_gateway,
-              "availability_zones": ($deployment_availability_zones | split(","))
-            }
-          ]
-        },
-        {
-          "name": $services_network_name,
-          "service_network": $services_network_is_service_network,
-          "subnets": [
-            {
-              "iaas_identifier": $services_vcenter_network,
-              "cidr": $services_network_cidr,
-              "reserved_ip_ranges": $services_reserved_ip_ranges,
-              "dns": $services_dns,
-              "gateway": $services_gateway,
-              "availability_zones": ($services_availability_zones | split(","))
             }
           ]
         }
@@ -178,7 +135,7 @@ om-linux \
   --iaas-configuration "$iaas_configuration" \
   --director-configuration "$director_config" \
   --az-configuration "$az_configuration" \
-#  --networks-configuration "$network_configuration"  
+  --networks-configuration "$network_configuration"  
 #   --iaas-configuration "$iaas_configuration" \
 #--security-configuration "$security_configuration" \
 #--resource-configuration "$resource_configuration" \
