410a99413db2f60dd08b7ff47d2f3254df4138d6
Igor Fomenko <igor.fomenko@logimethods.com>
2018-01-16 19:12:52 -0500

    Changing file permissions

--- pcf-deployment-ci/pcf-terraform-gcp/ci/tasks/config-director/task.sh
+++ pcf-deployment-ci/pcf-terraform-gcp/ci/tasks/config-director/task.sh
@@ -175,13 +175,13 @@ om-linux \
   --username $OPS_MGR_USR \
   --password $OPS_MGR_PWD \
   configure-bosh \
-  --iaas-configuration "$iaas_configuration" \
-  --director-configuration "$director_config" \
-  --az-configuration "$az_configuration" \
-  --networks-configuration "$network_configuration" 
+  --iaas-configuration "$iaas_configuration" 
+
  
   
-  
+#   --director-configuration "$director_config" \
+#  --az-configuration "$az_configuration" \
+#  --networks-configuration "$network_configuration"  
 #   --iaas-configuration "$iaas_configuration" \
 #--security-configuration "$security_configuration" \
 #--resource-configuration "$resource_configuration" \
