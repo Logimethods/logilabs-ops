bf9625d606365cbb666af0fa8888583b27c98207
Igor Fomenko <igor.fomenko@logimethods.com>
2018-01-17 19:00:42 -0500

    Changing file permissions

--- pcf-deployment-ci/pcf-terraform-gcp/ci/tasks/config-director/task.sh
+++ pcf-deployment-ci/pcf-terraform-gcp/ci/tasks/config-director/task.sh
@@ -40,10 +40,10 @@ network_configuration=$(
     --argjson icmp_checks_enabled false \
     --arg infra_network_name "pcf-meetup-pcf-network" \
     --arg infra_vcenter_network "$GCP_PROJECT_ID/${GCP_RESOURCE_PREFIX}-pcf-network/${GCP_RESOURCE_PREFIX}-management-subnet/${GCP_REGION}" \
-    --arg infra_network_cidr "10.0.0.0/24" \
-    --arg infra_reserved_ip_ranges "10.0.0.1-10.0.0.10" \
-    --arg infra_dns "10.0.0.1,8.8.8.8" \
-    --arg infra_gateway "10.0.0.1" \
+    --arg infra_network_cidr "10.0.6.0/24" \
+    --arg infra_reserved_ip_ranges "10.0.6.1-10.0.6.10" \
+    --arg infra_dns "10.0.6.1,8.8.8.8" \
+    --arg infra_gateway "10.0.6.1" \
     --arg infra_availability_zones "$availability_zones" \
     '
     {
