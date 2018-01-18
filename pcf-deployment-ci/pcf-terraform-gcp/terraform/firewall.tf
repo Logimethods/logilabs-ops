15a984773873992cb0f5e49f44ca5bc9d92545d7
Igor Fomenko <igor.fomenko@logimethods.com>
2018-01-18 10:01:06 -0500

    firewall

---
+++ pcf-deployment-ci/pcf-terraform-gcp/terraform/firewall.tf
@@ -0,0 +1,27 @@
+## Ops manager to director
+resource "google_compute_firewall" "pcf-allow-director" {
+  name    = "${var.env_name}-allow-director"
+  network = "pcf-meetup-pcf-network"
+
+  allow {
+    protocol = "all"
+  }
+
+  source_ranges = ["10.0.6.0/24"]
+}
+
+# Allow HTTP/S access to Ops Manager from the outside world
+resource "google_compute_firewall" "ops-manager-external" {
+  name        = "${var.env_name}-ops-manager-external"
+  network     = "${google_compute_network.pcf-network.name}"
+  target_tags = ["${var.env_name}-ops-manager-external"]
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+  }
+}
--- pcf-deployment-ci/pcf-terraform-gcp/terraform/ops_manager.tf
+++ pcf-deployment-ci/pcf-terraform-gcp/terraform/ops_manager.tf
@@ -1,18 +1,4 @@
-# Allow HTTP/S access to Ops Manager from the outside world
-resource "google_compute_firewall" "ops-manager-external" {
-  name        = "${var.env_name}-ops-manager-external"
-  network     = "${google_compute_network.pcf-network.name}"
-  target_tags = ["${var.env_name}-ops-manager-external"]
 
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22", "80", "443"]
-  }
-}
 
 
 #resource "google_compute_address" "ops-manager-ip" {
